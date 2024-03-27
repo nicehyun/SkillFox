bb493618b3d8a76af820aad66e09e5c81be97464
ish <lshsh5656@naver.com>
2024-03-27 23:15:10 +0900

    Feat : tailwind.config 스타일 가이드 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
 export default function Home() {
-  return <div>testasd</div>;
+  return (
+    <div>
+      <div className="h-[250px] w-[250px] bg-primary"></div>
+      <div className="mb-4 mt-4 h-[250px] w-[250px] bg-secondary"></div>
+      <div className="bg-info h-[250px] w-[250px]"></div>
+      <div className="h-[250px] w-[250px] bg-success"></div>
+      <div className="bg-warning h-[250px] w-[250px]"></div>
+      <div className="h-[250px] w-[250px] bg-error"></div>
+      <div className="bg-black1 h-[250px] w-[250px]"></div>
+      <div className="bg-black2 h-[250px] w-[250px]"></div>
+      <div className="bg-black3 h-[250px] w-[250px]"></div>
+      <div className="h-[250px] w-[250px] bg-white"></div>
+      <div className="bg-gray1 h-[250px] w-[250px]"></div>
+      <div className="bg-gray2 h-[250px] w-[250px]"></div>
+      <div className="bg-gray3 h-[250px] w-[250px]"></div>
+
+      <h1 className="text-h1">Heading 1</h1>
+      <h2 className="text-h2">Heading 2</h2>
+      <h3 className="text-h3">Heading 3</h3>
+      <h4 className="text-h4">Heading 4</h4>
+      <h5 className="text-h5">Heading 5</h5>
+      <h6 className="text-h6">Heading 6</h6>
+
+      <p className="text-large">Large Text Bold</p>
+      <p className="text-medium">Medium Text Regular</p>
+      <p className="text-normal">Normal Text Bold</p>
+      <p className="text-small">Small Text Regular</p>
+      <p className="text-[40px]">Small Text Regular</p>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,8 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: ["class", '[data-mode="dark"]'],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       backdropBlur: {
         xs: "2px",
       },
@@ -18,27 +12,55 @@ const config: Config = {
       },
     },
 
+    fontSize: {
+      h1: ["56px", "61.6px"],
+      h2: ["48px", "52.8px"],
+      h3: ["40px", "44px"],
+      h4: ["32px", "35.2px"],
+      h5: ["24px", "26.4px"],
+      h6: ["20px", "22px"],
+      large: ["20px", "28px"],
+      medium: ["18px", "25.2px"],
+      normal: ["16px", "22.4px"],
+      small: ["14px", "19.6px"],
+    },
+
+    spacing: {
+      "2": "8px",
+      "4": "16px",
+      "6": "24px",
+      "8": "32px",
+      "10": "40px",
+      "14": "56px",
+      "18": "72px",
+      "20": "80px",
+      "24": "96px",
+      "30": "120px",
+    },
+
     screens: {
-      sm: { max: "479px" },
-      md: { min: "480px", max: "767px" },
-      lg: { min: "768px", max: "1023px" },
-      xl: { min: "1024px" },
+      sm: { max: "320px" },
+      md: { min: "321px", max: "768px" },
+      lg: { min: "769px", max: "1024px" },
+      xl: { min: "1025px", max: "1440px" },
     },
 
     colors: {
       transparent: "transparent",
-      primary: "#30C97A",
-      secondary: "#303640",
-      tertiary: "#fa4c3d",
-      white: "#FFF",
-      black: "#333",
-      mono1: "#9E9E9E",
-      mono2: "#3e4042",
-      border: "#dee2e6",
-      success: "#4CAF50",
-      error: "#B00020",
+      primary: "#F2994A",
+      secondary: "#092C4C",
+      info: "#2F80ED",
+      success: "#27AE60",
+      warning: "#E2B93B",
+      error: "#EB5757",
+      black1: "#000000",
+      black2: "#1C1C1C",
+      black3: "#333333",
+      white: "#FFFFFF",
+      gray1: "#828282",
+      gray2: "#BDBDBD",
+      gray3: "#E0E0E0",
     },
   },
-  plugins: [],
 };
 export default config;
