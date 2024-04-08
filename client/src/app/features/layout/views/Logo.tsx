f720aae23edbe2a8a50f6bda6372cc9112cdf72d
ish <lshsh5656@naver.com>
2024-04-09 01:46:56 +0900

    Design : Logo css 수정

--- client/src/app/features/layout/views/Logo.tsx
+++ client/src/app/features/layout/views/Logo.tsx
@@ -3,18 +3,27 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <h1>
-      <Link href="/" className="relative inline-block h-[30px] w-[30px]">
-        <Image
-          alt="skillfox-logo"
-          src="/images/logo.webp"
-          fill
-          priority
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="absolute"
-        />
-      </Link>
-    </h1>
+    <Link href="/">
+      <h1 className="flex">
+        <span className="mr-2 inline-block rounded-[5px] border-[1px] border-gray1 p-[2px]">
+          <span className="flexCenter relative h-[26px] w-[26px]">
+            <Image
+              alt="skillfox-logo"
+              src="/images/logo.webp"
+              fill
+              priority
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="absolute"
+            />
+          </span>
+        </span>
+
+        <div className="flex flex-col text-xs">
+          <span className="font-bold">SKILL FOX</span>
+          <span className="text-gray1">채용 공고 기술 분석</span>
+        </div>
+      </h1>
+    </Link>
   );
 };
 
