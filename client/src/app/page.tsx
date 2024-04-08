f34ed54b11b5fccea393fb0d191eacb67d5e1563
ish <lshsh5656@naver.com>
2024-04-09 01:07:41 +0900

    Feat : 직무별 link 추가

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,39 @@
 import Link from "next/link";
 
 export default function Home() {
+  const links = [
+    {
+      id: "FE",
+      content: "프론트엔드",
+    },
+    {
+      id: "BE",
+      content: "백엔드・서버",
+    },
+    {
+      id: "DE",
+      content: "데이터 엔지니어",
+    },
+    {
+      id: "DA",
+      content: "데이터 분석가",
+    },
+    {
+      id: "ML",
+      content: "머신러닝 엔지니어",
+    },
+  ];
   return (
     <section className="flex flex-col">
-      <Link href="/FE/skillFrequency">프론트엔드</Link>
-      <Link href="/BE/skillFrequency">백엔드・서버</Link>
-      <Link href="/DE/skillFrequency">데이터 엔지니어</Link>
-      <Link href="/DA/skillFrequency">데이터 분석가</Link>
-      <Link href="/ML/skillFrequency">머신러닝 엔지니어</Link>
+      {links.map((link, index) => (
+        <Link
+          key={`link-${link.id}`}
+          className={`${index !== 0 ? "mt-6" : ""} flexCenter w-full rounded-[5px] border-[2px] border-orange py-4 font-bold shadow`}
+          href={`/${link.id}/skillFrequency`}
+        >
+          {link.content}
+        </Link>
+      ))}
     </section>
   );
 }
