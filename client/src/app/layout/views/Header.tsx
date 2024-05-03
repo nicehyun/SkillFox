3fe8e773052ed8a41dec88b48708fe1950c6df37
ish <lshsh5656@naver.com>
2024-05-03 22:25:00 +0900

    Refactor : home page 에서 AnalysisNavigationController 제거

--- client/src/app/layout/views/Header.tsx
+++ client/src/app/layout/views/Header.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import AnalysisNavigationController from "./AnalysisNavigationController";
 import Logo from "./Logo";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <header className="fixed z-30 flex h-[80px] w-full items-center justify-between border-b-[1px] border-gray2 bg-white/50 px-4 backdrop-blur-[4px]">
       <Logo />
-
-      <AnalysisNavigationController />
+      {pathname !== "/" && <AnalysisNavigationController />}
     </header>
   );
 };
