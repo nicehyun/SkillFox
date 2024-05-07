a04e8e1ffbdff7204427f648c8febe7e026f926d
ish <lshsh5656@naver.com>
2024-05-07 22:37:24 +0900

    Refactor : gitignore 수정

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import "../src/app/globals.css";
+
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+      expanded: true,
+    },
+  },
+};
+
+export default preview;
