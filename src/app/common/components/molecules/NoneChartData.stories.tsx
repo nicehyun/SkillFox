5b03deeb5ef19863ec7c695672041ff92f68b7b0
ish <lshsh5656@naver.com>
2024-05-09 23:11:13 +0900

    Feat : NoneChartData Storybook

---
+++ src/app/common/components/molecules/NoneChartData.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from "@storybook/react";
+import NoneChartData from "./NoneChartData";
+
+const meta: Meta = {
+  title: "Common/molecules/NoneChartData",
+  component: NoneChartData,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof meta> = {};
--- src/app/common/components/molecules/NoneChartData.tsx
+++ src/app/common/components/molecules/NoneChartData.tsx
@@ -1,6 +1,13 @@
 const NoneChartData = () => {
   return (
-    <div className="flexCenter mx-auto mt-30  h-30 w-full rounded-lg border border-border p-4 text-center shadow sm:text-small md:text-small lg:max-w-[30%] xl:max-w-[30%]">
+    <div
+      // 분석 결과가 없는 것은 사용자에게 중요한 변경 사항일 수 있으므로 이 역할이 적절
+      role="alert"
+      aria-live="polite"
+      // aria-live가 활성화된 요소에서 전체 요소의 내용이 변경될 때마다 전체적으로 내용을 다시 읽어야 함
+      aria-atomic="true"
+      className="flexCenter mx-auto mt-30  h-30 w-full rounded-lg border border-border p-4 text-center shadow sm:text-small md:text-small lg:max-w-[40%] xl:max-w-[30%]"
+    >
       분석 결과가 존재하지 않습니다.
     </div>
   );
