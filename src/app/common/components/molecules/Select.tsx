10d83c8ef0e953c4c4a0cd8c1561c413602eb9b6
ish <lshsh5656@naver.com>
2024-05-09 23:37:04 +0900

    Feat : aria 추가

--- src/app/common/components/molecules/Select.tsx
+++ src/app/common/components/molecules/Select.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { KeyboardEvent, useEffect, useRef, useState } from "react";
 import { FaAngleDown } from "react-icons/fa6";
 import Icon from "../atoms/Icon";
 import useOutsideClick from "../../hooks/useOutSideClick";
@@ -31,6 +31,19 @@ export const CustomSelect = ({
     setIsOpen(false);
   };
 
+  const handleSelectToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleEnterKeyDown = (
+    event: KeyboardEvent<HTMLDivElement>,
+    cb: () => void,
+  ) => {
+    if (event.key === "Enter") {
+      cb();
+    }
+  };
+
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       const target = event.target as Node;
@@ -51,14 +64,19 @@ export const CustomSelect = ({
     <div
       ref={wrapperRef}
       className={`${className} relative inline-block`}
-      role="listbox"
-      aria-activedescendant={`select-option-${id}-${selectValue}`}
       aria-expanded={isOpen}
+      // 셀렉트 박스를 활성화할 때 리스트박스가 표시될 것임을 나타냄
+      aria-haspopup="listbox"
+      // 셀렉트 박스의 레이블을 식별하는 데 사용됩니다. 이 컴포넌트의 경우, 사용자가 선택한 값을 표시하는 div 요소에 레이블이 적절히 제공되도록 설정
+      aria-labelledby={`select-label-${id}`}
     >
       <div
-        className="flex w-full items-center justify-between rounded-[5px] bg-border p-[5px] text-xs text-black2 shadow hover:cursor-pointer focus:outline-none"
-        onClick={() => setIsOpen((prev) => !prev)}
+        className="flex w-full items-center justify-between rounded-[5px] border-[1px] border-border bg-border p-[5px] text-xs text-black2 shadow hover:cursor-pointer focus:border-primary focus:outline-none"
+        onClick={handleSelectToggle}
         id={`select-label-${id}`}
+        // 키보드 접근성 추가
+        tabIndex={0}
+        onKeyDown={(event) => handleEnterKeyDown(event, handleSelectToggle)}
       >
         {`${defalutSelectContent} : `}
         <strong>{selectValue}</strong>
@@ -66,15 +84,25 @@ export const CustomSelect = ({
       </div>
 
       {isOpen && (
-        <div className="absolute z-20 mt-2 w-full rounded-[5px] bg-white text-xs shadow">
+        <div
+          className="absolute z-20 mt-2 w-full rounded-[5px] bg-white text-xs shadow"
+          id={`select-list-${id}`}
+          // 토글된 엘리먼트(여기서는 옵션 목록)의 ID
+          aria-controls={`select-list-${id}`}
+        >
           {options.map((option, index) => (
             <div
               key={index}
               className="cursor-pointer px-4 py-2 hover:bg-border"
               onClick={() => handleSelect(option)}
+              onKeyDown={(event) =>
+                handleEnterKeyDown(event, () => handleSelect(option))
+              }
               role="option"
               id={`select-option-${id}-${option}`}
+              // 요소가 현재 선택된 상태인지 아닌지
               aria-selected={selectValue === option}
+              tabIndex={0}
             >
               {`${option}개 표시하기`}
             </div>
