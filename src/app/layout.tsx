e5c9f0142cae99ef6e0fef639175cbd82ce76486
ish <lshsh5656@naver.com>
2024-05-20 20:53:53 +0900

    Feat : default meta data 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from "./layout/components/organisms/Footer";
 import Header from "./layout/components/organisms/Header";
 import Providers from "./common/utils/Providers";
 import { Metadata } from "next";
+import Head from "next/head";
 
 const notoSansKr = Noto_Sans_KR({
   subsets: ["latin"],
@@ -11,10 +12,33 @@ const notoSansKr = Noto_Sans_KR({
 });
 
 export const metadata: Metadata = {
+  generator: "Next.js",
+  applicationName: "SkillFox",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "채용공고",
+    "기술",
+    "분석",
+    "프론트엔드",
+    "백엔드",
+    "데이터 엔지니어",
+    "데이터 분석가",
+    "머신러닝",
+  ],
+  authors: [{ name: "Noah", url: "https://github.com/nicehyun" }],
+  creator: "Noah Lee",
+  metadataBase: new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`),
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    images: "/public/images/logo.webp",
+  },
   title: {
-    default: "SkillFox | 채용공고 기술스택 분석",
+    default: "SkillFox | 채용공고 기술 분석",
     template: "SkillFox | %s",
   },
+  description: "채용공고 자격 요건 기술 스택 분석",
 };
 
 export default function RootLayout({
